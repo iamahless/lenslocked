eb70f3c61da2ef8ce3920d5541a059d1428fffbe
Alexander <alexandergaruba96@gmail.com>
2022-10-26 00:57:06 +0100

    faq page

--- main.go
+++ main.go
@@ -15,7 +15,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please email me at <a href=\"mailto:alexandergaruba96@gmail.com\">alexandergaruba96@gmail.com</a>.</p>")
+	_, err := fmt.Fprint(w, ""+
+		"<h1>Contact Page</h1>"+
+		"<p>To get in touch, please email me at "+
+		"<a href=\"mailto:alexandergaruba96@gmail.com\">alexandergaruba96@gmail.com</a>."+
+		"</p>")
+	if err != nil {
+		return
+	}
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "<h1>FAQ Page</h1>"+
+		"<ul>"+
+		"<li><b>Is there a free version?</b>Yes! We offer a free trial for 30 days on any paid plans.</li>"+
+		"<li><b>What are your support hours?</b>We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</li>"+
+		"<li><b>How do I contact support?</b>Email me - <a href=\"mailto:alexandergaruba96@gmail.com\">alexandergaruba96@gmail.com</a></li></ul>")
 	if err != nil {
 		return
 	}
@@ -27,6 +43,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 //		homeHandler(w, r)
 //	case "/contact":
 //		contactHandler(w, r)
+//	case "/faq":
+//		faqHandler(w, r)
 //	default:
 //		http.Error(w, "Page not found!", http.StatusNotFound)
 //	}
@@ -40,6 +58,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found!", http.StatusNotFound)
 	}
