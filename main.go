27548c4519b2c7f848c176897e2b3ca03ae415af
Alexander <alexandergaruba96@gmail.com>
2022-10-25 22:53:13 +0100

    contact page

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	if err != nil {
@@ -13,8 +13,17 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please email me at <a href=\"mailto:alexandergaruba96@gmail.com\">alexandergaruba96@gmail.com</a>.</p>")
+	if err != nil {
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
